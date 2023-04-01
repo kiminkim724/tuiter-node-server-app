70e3b466f7d3ca6aebf5a88eb51b84a796bee640
kiminkim724 <kim.ki@husky.neu.edu>
2023-04-01 19:07:42 -0400

    first commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        "_id": "123", "topic": "Space", "username": "SpaceX",
+        "handle": "@spacex", "time": "2h", "image": "https://infosec-jobs.com/media/company/logo/20/03/spacex.jpg",
+        "title": "SpaceX's Mission",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "liked": true, "likes": 2345, "disliked": true, "dislikes": 2345, "replies": 123, "retuits": 432
+    },
+    {
+        "_id": "234", "topic": "Traffic", "username": "The Boring Company",
+        "handle": "@boringcompany", "time": "2h", "image": "https://i.pinimg.com/originals/ff/d3/6c/ffd36ce1af1b8b3c6edce349a07046f3.jpg",
+        "title": "The Boring Company fixing traffic",
+        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+        "liked": true, "likes": 2345, "disliked": false, "dislikes": 2345,"replies": 321, "retuits": 432
+    },
+    {
+        "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
+        "handle": "@neurolink", "time": "3h", "image": "https://upload.wikimedia.org/wikipedia/commons/4/44/Neuralink_logo.svg",
+        "title": "Neurolink fixes paralisis and blindness",
+        "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
+        "liked": true, "likes": 3456, "disliked": true, "dislikes": 2345, "replies": 345, "retuits": 654
+    },
+];
